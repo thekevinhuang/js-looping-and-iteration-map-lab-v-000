99549912fb455404ce13493d005106bf69bd94f4
Kevin Huang <yellow.khaos@gmail.com>
2018-10-05 22:09:55 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 // Code your solution in this file.
 function lowerCaseDrivers(collection) {
+  let newCollect = collection.map(function(individual) {individual.toLowerCase()})
+}
+
+function nameToAttributes(collection) {
   
 }
